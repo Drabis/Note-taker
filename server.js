be0193ce9426db2edc9136793ef0bac573e97590
Drabis <drabis47@gmail.com>
2021-04-14 09:36:50 -0400

    server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const app = express(); 
+
+// Set an initial PORT
+
+let PORT = process.env.PORT || 8000;
+
+// Set up handle data parsing files
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json()); 
+app.use( express.static('public'));
+
+
+// IMPORT ROUTES - ORDER IS IMPORTANT 
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+
+// SET UP LISTEN EVENT ON THE SERVER 
+
+app.listen(PORT, () => {
+  console.log(`App listening on PORT ${PORT}` );
+})
